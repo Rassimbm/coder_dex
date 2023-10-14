6c8f64a7bfc0f96bb984b6c9169c630148f49635
Rassimbm <rassimbenmhamed@outlook.com>
2023-10-14 16:17:10 -0500

    updated script

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function getUserName(element){
     // console.log(element.value);
     currentUserName = element.value;
 }
+
+function makeUserCard(data){
+    var response = `<div class="card">
+                        <img src="${data.avatar_url} alt="${data.type}">
+                        <div class="flex-1">
+                            <h3>Name: ${data.name}</h3>
+                            <h5>Location: ${data.location}</h5>
+                            <p>Repositories: ${data.public_repos}</p>
+                        </div>
+                    </div>`;
+
+    return response;
+}
+
+async function search(){
+    var response = await fetch("https://api.github.com/users/" + currentUserName);
+    var userData = await response.json();
+    // console.log(userData);
+    cardsDiv.innerHTML = makeUserCard(userData) + cardsDiv.innerHTML;
+}
